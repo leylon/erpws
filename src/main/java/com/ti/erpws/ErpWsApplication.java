60fa8e8c0b68061f68aadafd0ebe153cc367a8d3
leylon ocaña Sanchez 2 <leylon257@gmail.com>
2020-10-11 18:44:35 -0500

    initial commit

---
+++ src/main/java/com/ti/erpws/ErpWsApplication.java
@@ -0,0 +1,13 @@
+package com.ti.erpws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ti/erpws/ErpWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti.erpws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
